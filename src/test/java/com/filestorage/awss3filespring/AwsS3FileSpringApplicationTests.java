c8a687fc4b7e9cf949970c1763ad9582d8388e3c
aek <konan.kouame@soprasteria.com>
2021-08-14 17:04:00 +0200

    initial commit

---
+++ src/test/java/com/filestorage/awss3filespring/AwsS3FileSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filestorage.awss3filespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3FileSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
